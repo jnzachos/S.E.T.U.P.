05349f9684c7aea824e947904301a1b7753c0fe1
jnzachos <jonijellyfish@gmail.com>
2026-08-18 19:00:00 +0300

    Fixed netcat package

--- hacking-setup/modules/net.sh
+++ hacking-setup/modules/net.sh
@@ -1,6 +1,8 @@
-run_recon() {
+#! /usr/bin/env bash
+
+run_net() {
     log "Setting up networking tools..."
     install_apt socat
     install_apt wireshark
-    install_apt netcat
+    install_apt netcat-openbsd
 }
