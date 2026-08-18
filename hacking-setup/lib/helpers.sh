f2eaf58de0d968ee126a251c209b241b257c03c3
jnzachos <jonijellyfish@gmail.com>
2026-08-18 18:37:30 +0300

    small error fix

--- hacking-setup/lib/helpers.sh
+++ hacking-setup/lib/helpers.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
  
-#log()  { echo -e "\e[1;32m[+]\e[0m $*"; }
-#info() { echo -e "\e[1;34m[i]\e[0m $*"; } 
-#warn() { echo -e "\e[1;33m[!]\e[0m $*"; }
-#err()  { echo -e "\e[1;31m[-]\e[0m $*" >&2; }
+log()  { echo -e "\e[1;32m[+]\e[0m $*"; }
+info() { echo -e "\e[1;34m[i]\e[0m $*"; } 
+warn() { echo -e "\e[1;33m[!]\e[0m $*"; }
+err()  { echo -e "\e[1;31m[-]\e[0m $*" >&2; }
 
 
 install_apt() {
