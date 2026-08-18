3a2068dd181add2231cc69a02c581db0433570e2
jnzachos <jonijellyfish@gmail.com>
2026-08-18 22:47:58 +0300

    fix

--- hacking-setup/modules/recon.sh
+++ hacking-setup/modules/recon.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 run_recon() {
-    log "Setting up reconnaisanc tools..."
+    log "Setting up reconnaisance tools..."
     install_apt nmap
     install_apt gobuster
     install_apt ffuf
